d928713e8d05b94134379804b116f91942b9dc2a
Aastha Yadav <aasthayadav2055@gmail.com>
2026-05-11 23:41:46 +0530

    app.py

--- app.py
+++ app.py
@@ -538,40 +538,29 @@ if st.button("✨ Generate AI Fraud Analysis"):
 
         ai_output = response.choices[0].message.content
 
-        st.markdown("## 🤖 AI Fraud Intelligence Report")
-
-        st.markdown(f"""
+       st.markdown("""
 <div style="
-
 background: linear-gradient(
 135deg,
 #FFF1F7 0%,
 #FFE4F1 100%
 );
-
 padding: 28px;
-
 border-radius: 24px;
-
 border-left: 8px solid #EC4899;
-
-box-shadow:
-0px 6px 20px rgba(236,72,153,0.12);
-
+box-shadow: 0px 6px 20px rgba(236,72,153,0.12);
 margin-top: 20px;
-
 color: #1E293B;
-
 line-height: 1.8;
-
 font-size: 16px;
-
 ">
-
-{ai_output}
-
+<h3 style="color:#BE185D;">
+🤖 AI Fraud Intelligence Report
+</h3>
 </div>
 """, unsafe_allow_html=True)
+
+st.write(ai_output)
 
         # DOWNLOAD REPORT
 
